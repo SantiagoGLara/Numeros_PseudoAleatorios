dcbbfaba7c04f9003723b6805135e7817640d284
SantiagoGLara <relived57@gmail.com>
2024-10-14 13:03:56 -0600

    correccion, ya sobrecarga correctamente los metodos abstractos/ inicio ventanas

---
+++ src/VentanaGeneracion.java
@@ -0,0 +1,9 @@
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+public class VentanaGeneracion extends JFrame{
+
+    public VentanaGeneracion(){
+        super("Generador Medios Cuadrados");
+    }
+}

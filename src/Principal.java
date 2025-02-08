929869ef6967df97b1531f0f9c2ad1d69b4089b8
SantiagoGLara <relived57@gmail.com>
2025-02-07 20:37:58 -0600

    proyecto simulacion

--- src/Principal.java
+++ src/Principal.java
@@ -1,7 +1,7 @@
 public class Principal {
     public static void main(String[] args) throws Exception {
         //hacer programa con UI para numeros pseudoaleatorios
-        NumeroPseudoaleatorio numero = new CuadradosMedios();
+       // NumeroPseudoaleatorio numero = new CuadradosMedios();
         CuadradosMedios numero1 = new CuadradosMedios();
         numero1.setNumero(5735.0);
         System.out.println(numero1.generarNumero());
--- src/VentanaGeneracion.java
+++ src/VentanaGeneracion.java
@@ -10,14 +10,11 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
-import java.awt.Dimension;
-import javax.swing.border.Border;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JTextField;
 import javax.swing.BoxLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
 import javax.swing.table.DefaultTableModel;
 
 public class VentanaGeneracion extends JFrame{
